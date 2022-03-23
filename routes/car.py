370f56f0d05bafc17bbc6b3138eb97dd7c78fd9e
Lucas Masiero <lucas.masi.ero@hotmail.com>
2022-03-23 11:56:55 -0300

    full api

--- routes/car.py
+++ routes/car.py
@@ -14,21 +14,8 @@ CarService = APIRouter(
 )
 
 # Read
-@CarService.get("/get/{id}", response_model=CarResponse)
-async def get_car(id:int, db:Session=Depends(get_db)):
-    # Process
-    car = db.query(Car).filter(Car.id == id).first()
-    if car is None:
-        raise HTTPException(status_code=400, detail=Response(
-            message="Failed To Find Car!",
-            data=None
-        ))
-        
-    # Response
-    return CarResponse.from_orm(car)
-
-@CarService.get("/", response_model=Response)
-async def get_car_name(car_plate: str = "", db:Session=Depends(get_db)):
+@CarService.get("/get/{car_plate}", response_model=CarResponse)
+async def get_car(car_plate: str, db:Session=Depends(get_db)):
     # Process
     car = db.query(Car).filter(Car.car_plate == car_plate.upper()).first()
     if car is None:
@@ -38,11 +25,23 @@ async def get_car_name(car_plate: str = "", db:Session=Depends(get_db)):
         ).dict())
     
     # Response
-    return Response(
-        message="Succeed To Find Car!",
-        data=car.car_name
-    )
+    return CarResponse.from_orm(car)
 
+# @CarService.get("/", response_model=Response)
+# async def get_car_name(car_plate: str = "", db:Session=Depends(get_db)):
+#     # Process
+#     car = db.query(Car).filter(Car.car_plate == car_plate.upper()).first()
+#     if car is None:
+#         raise HTTPException(status_code=400, detail=Response(
+#             message="Failed To Find Car!",
+#             data=None
+#         ).dict())
+    
+#     # Response
+#     return Response(
+#         message="Succeed To Find Car!",
+#         data=car.car_name
+#     )
 
 # Write
 @CarService.post("/create", response_model=CarResponse)
